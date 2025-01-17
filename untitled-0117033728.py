af47b9498d92a88b30e0234d44abf8564fefd601
coder32768 <rick.taylor3@outlook.com>
2025-01-16 19:37:29 -0800

    Pushed DAG untitled-0117033728

---
+++ untitled-0117033728.py
@@ -0,0 +1,51 @@
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.kubernetes.secret import Secret
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "untitled-0117033728",
+}
+
+dag = DAG(
+    "untitled-0117033728",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.15.0 pipeline editor using `untitled.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: Untitled.ipynb
+
+op_5ee470c4_46c1_4bb9_8441_cadefa60264e = KubernetesPodOperator(
+    name="Untitled",
+    namespace="default",
+    image="continuumio/anaconda3@sha256:a2816acd3acda208d92e0bf6c11eb41fda9009ea20f24e123dbf84bb4bd4c4b8",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'untitled' --cos-endpoint http://192.168.1.33:59333 --cos-bucket elyra --cos-directory 'untitled-0117033728' --cos-dependencies-archive 'Untitled-5ee470c4-46c1-4bb9-8441-cadefa60264e.tar.gz' --file 'Untitled.ipynb' "
+    ],
+    task_id="Untitled",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "elyra",
+        "AWS_SECRET_ACCESS_KEY": "93077529307752",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "untitled-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
